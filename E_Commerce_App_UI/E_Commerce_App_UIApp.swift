638311c9a125a878a534c44fdb4007a4045ec9d4
Faruq <faruqdeveloper@gmail.com>
2021-11-29 22:36:36 +0600

    Initial Commit

---
+++ E_Commerce_App_UI/E_Commerce_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_App_UIApp.swift
+//  E_Commerce_App_UI
+//
+//  Created by Md Omar Faruq on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ E_Commerce_App_UITests/E_Commerce_App_UITests.swift
@@ -0,0 +1,33 @@
+//
+//  E_Commerce_App_UITests.swift
+//  E_Commerce_App_UITests
+//
+//  Created by Md Omar Faruq on 11/29/21.
+//
+
+import XCTest
+@testable import E_Commerce_App_UI
+
+class E_Commerce_App_UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
